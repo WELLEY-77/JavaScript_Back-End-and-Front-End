6842287498b71750dbac9023c59cbc7653a7cb5b
Welley <monteirowelley7@gmail.com>
2023-08-30 16:35:30 -0300

    ultilizando Set();

---
+++ Set.js
@@ -0,0 +1,5 @@
+const nomes = ['pedro', 'pedro', 'maria', 'joao', 'joao', 'clara'];
+
+const nomesAtualizados = [...new Set(nomes)];
+
+console.log(nomesAtualizados)
---
+++ copiando-array.js
@@ -0,0 +1,8 @@
+const notas = [5, 10, 5, 9]
+
+const novasNotas = [...notas]
+
+novasNotas.push(10)
+
+console.log(`As novas notas são ${novasNotas}`)
+console.log(`As notas originais são ${notas}`)
