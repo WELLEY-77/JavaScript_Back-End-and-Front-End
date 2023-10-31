9679f1f0aba9e066f40189932ee9d6a5c4bc3f35
Welley <monteirowelley7@gmail.com>
2023-10-31 20:24:12 -0300

    eventos DOM

---
+++ eventos-DOM/index.js
@@ -0,0 +1,19 @@
+function onMouseEnter() {
+    console.log('Hello World')
+}
+
+function onChange() {
+    window.alert('Ola ...')
+}
+
+function onFocus() {
+    console.log('hello')
+}
+
+function onBlur() {
+    console.log('lerolero')
+}
+
+function olaMundo() {
+    console.log('ola mundo')
+}
